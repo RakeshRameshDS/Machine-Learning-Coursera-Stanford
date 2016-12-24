8770f21b28687c664ef95110c2c08a08d012ca10
Rakesh Ramesh <rakeshr@usc.edu>
2016-12-24 09:10:14 -0800

    Code Base Commit
    Uploading code

---
+++ machine-learning-ex6/ex6_SVMTrain/dataset3Params.m
@@ -0,0 +1,48 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+min_error = 999;
+min_c = 0;
+min_sigma = 0;
+
+C_List = [0.01, 0.03, 0.1, 0.3, 1, 3 , 10, 30];
+sigma_List = [0.01, 0.03, 0.1, 0.3, 1, 3 , 10, 30];
+%C_List = [0.1,0.13,0.16,0.19,0.2,0.22,0.25];
+%sigma_List = [0.06,0.063,0.066,0.07,0.073,0.076,0.08];
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+for i=1:length(C_List)
+    for j=1:length(sigma_List)
+        Options = ['-s 0 -t 3 -g', ' ', num2str(sigma_List(i)),' -c ',num2str(C_List(i))];
+        model= svmtrain(y, X, Options);
+        predictions = svmpredict(yval, Xval, model);
+        err = mean(double(predictions ~= yval));
+        if err<min_error
+            min_error = err;
+            min_c = C_List(i);
+            min_sigma = sigma_List(j);
+        end
+    end
+end
+C = min_c;
+sigma = min_sigma;
+end
---
+++ machine-learning-ex6/ex6_SVMTrain/temp_run.m
@@ -0,0 +1,8 @@
+% Load from ex6data3: 
+% You will have X, y in your environment
+load('ex6data3.mat');
+
+% Try different SVM Parameters here
+[C, sigma] = dataset3Params(X, y, Xval, yval);
+disp(C);
+disp(sigma);
